0cd68ca002959575941d1bdde67c5f0ccbea5300
marcski55 <marc.skwarczynski@revature.net>
2021-06-15 18:15:51 -0700

    Followed YouTube Tutorial to Learn How to Connect Express & DynamoDB.

---
+++ src/service.ts
@@ -0,0 +1,12 @@
+// Service Module: Complete Business Logic for Repo (to AWS w/ SDK)
+//* This file should only communicate between the controller and the database
+
+// TODO: Import database from DynamoDB
+
+// TODO: Read data from database
+
+// TODO: Add data to database
+
+// TODO: Modify data in database (Should modify be used or do delete/add?)
+
+// TODO: Delete entry from database
